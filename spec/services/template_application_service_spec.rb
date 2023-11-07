596b080a88e440b28b9050eff1b38c5751b32515
Petro Koriakin <petro.koriakin@homeflow.co.uk>
2023-11-07 03:57:03 +0200

    some nice specs

---
+++ spec/services/template_application_service_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TemplateApplicationService do
+  subject(:service_call) { described_class.new(template:) }
+
+  let(template) { List.templates.last }
+
+  before { Seeds::CreateDefaultTasklists.load_seed_data! }
+
+  it 'creates a list' do
+    expect { service_call }.to change { List.tasklists.count }.from(1).to(2)
+  end
+end
