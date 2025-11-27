df55d2b863c154795583a11ca29423defe87ed59
Petro Koriakin <petrokoriakin@gmail.com>
2025-11-27 18:41:06 +0700

    Fix tailwind purge

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,10 +2,12 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+    "./app/components/**/*.{rb,erb}",  // якщо використовуєте ViewComponents
+    "./app/**/*.html.erb",             // це найнадійніше — ловить усе
   ],
   theme: {
     extend: {
